b5077edc340e7d4ffab065a59c0a31a0324d8369
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-10-20 19:35:07 +0530

    Added:Intialize The Deck of Cards

---
+++ src/com/bridgelabz/workshop/DeckOfCards.java
@@ -0,0 +1,33 @@
+package com.bridgelabz.workshop;
+
+import java.util.ArrayList;
+public class DeckOfCards {
+
+	public ArrayList<String> cardsDeck = new ArrayList<>();
+
+	/*
+	    Simply created a Welcome Message Method without any return type.
+	 */
+
+	public void welcome() {
+		System.out.println("Welcome to the gaming world of Deck of Cards");
+	}
+
+	/*
+	    In this method we have initialized the size of cards.
+	    In this we are printing the size of card.
+	 */
+
+	public void deckOfCards() {
+		String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+		String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace"};
+		int numOfCards = suits.length * ranks.length;
+		System.out.println("\nNumber of cards in the deck:" + numOfCards);
+		for (int i = 0; i < ranks.length; i++) {       // First we have to itterate for loop for ranks starting from index 0.
+			for (int j = 0; j < suits.length; j++) {   // Now we have to ittreate the suits for all the indexes of ranks
+				cardsDeck.add(ranks[i] + "----->" + suits[j]);
+			}
+		}
+
+	}
+}
---
+++ src/com/bridgelabz/workshop/DeckOfCardsMain.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.workshop;
+
+public class DeckOfCardsMain {
+	 public static void main(String[] args) {
+	        /*
+	        In this I have created an object of DeckOfCardsGame class and just calling the method through object.
+	         */
+	        DeckOfCards deckOfCards = new DeckOfCards();
+	        deckOfCards.welcome();
+	        deckOfCards.deckOfCards();
+	        
+	    }
+}
