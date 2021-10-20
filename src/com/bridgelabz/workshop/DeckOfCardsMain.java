9b7cb3644cd541e1413fc25df43e39e4c5752562
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-10-20 20:20:29 +0530

    Added:Intialize The Deck of Cards

--- src/com/bridgelabz/workshop/DeckOfCardsMain.java
+++ src/com/bridgelabz/workshop/DeckOfCardsMain.java
@@ -3,11 +3,11 @@ package com.bridgelabz.workshop;
 public class DeckOfCardsMain {
 	 public static void main(String[] args) {
 	        /*
-	        In this I have created an object of DeckOfCardsGame class and just calling the method through object.
+	        In this I have created an object of DeckOfCards class and just calling the method through object.
 	         */
 	        DeckOfCards deckOfCards = new DeckOfCards();
 	        deckOfCards.welcome();
-	        deckOfCards.deckOfCards();
+	        deckOfCards.creatingDeckOfCards();
 	        
 	    }
 }
