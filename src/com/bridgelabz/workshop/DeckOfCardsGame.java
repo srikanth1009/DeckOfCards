98712c9403157242381beb25d3e8ffdcab57323d
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-10-25 14:18:32 +0530

    Added:Distributes the cards to all the player after Shuffling

--- src/com/bridgelabz/workshop/DeckOfCardsGame.java
+++ src/com/bridgelabz/workshop/DeckOfCardsGame.java
@@ -3,7 +3,7 @@ package com.bridgelabz.workshop;
 import java.util.ArrayList;
 import java.util.Scanner;
 /**
- * Purpose  - In this method we have defined the sequence of the player
+ * Purpose  - In this method we have distributes the cards to all the player after shuffling
  *
  */
 
@@ -93,5 +93,20 @@ public class DeckOfCardsGame {
         }
         cardsDeck = temp;
         toDisplay(cardsDeck);
+    }
+    /**
+    Created a method for equal distribution of cards.
+    Every player will be distributed 9 cards.
+     */
+    public static void cardDistribution(ArrayList<String> cardsDeck, int player) {
+            // This loop will itterate for no of players
+            for (int i = 0; i < player; i++) {
+            System.out.print("\nPlayer " + (i + 1) + " got cards:\n");
+            // This loop will itterate for no of cards for each player
+            for (int j = 0; j < 9; j++) {
+                System.out.print("\t" + cardsDeck.get(i+j*player));
+            }
+        }
+        System.out.println();
     }
 }
