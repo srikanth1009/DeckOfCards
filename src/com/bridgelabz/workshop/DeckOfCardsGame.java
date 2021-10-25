af9d997d0b98e1c857ef0bfb1f28f00f8e03461e
srikanth1009 <srikanthreddyyaddala8@gmail.com>
2021-10-25 12:43:26 +0530

    Added:There should be minimum 2 and maximum 4 players in the Game

--- src/com/bridgelabz/workshop/DeckOfCardsGame.java
+++ src/com/bridgelabz/workshop/DeckOfCardsGame.java
@@ -1,18 +1,30 @@
 package com.bridgelabz.workshop;
 
 import java.util.ArrayList;
-
+import java.util.Scanner;
 /**
- * Purpose  - We have to Initialize the size of Deck of cards
+ * Purpose  - There should be minimum 2 and maximum 4 players in the game
  *
  */
 
 public class DeckOfCardsGame {
+	public static final Scanner scanner = new Scanner(System.in);
 	public ArrayList<String> cardsDeck = new ArrayList<>();
+	
+	/**
+    Simply created a Welcome Message Method without any return type.
+     */
 
 	public void welcome(){
 		System.out.println("Welcome to the gaming world of Deck of Cards");
 	}
+	/**
+    In this method we have initialized the size of cards.
+    In this I have created String type array of Suits and Ranks to store its value and initialize it.
+    In this we are printing the size of card.
+    In this method we are also printing all the possible combination of card.
+     */
+
 	public void deckOfCards(){
 		String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
 		String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace"};
@@ -25,6 +37,11 @@ public class DeckOfCardsGame {
 		}
 		toDisplay(cardsDeck);
 	}
+	 /**
+    Created a display method to print the array list of cardsDeck.
+    We are using for-each loop to print all the element of ArrayList.
+    We are storing all the elements of cardsDeck arraylist in element.
+     */
 
 	public static void toDisplay(ArrayList<String> cardsDeck) {
 		System.out.println("\nCards in Deck:");
@@ -33,4 +50,22 @@ public class DeckOfCardsGame {
 		}
 		System.out.println();
 	}
+	
+	/**
+    In this we have to define no of players should be minimum 2 and maximum 4.
+    So we have use if condition to define no of players.
+    If no of player is not within the range then it will again call this method.
+     */
+
+    public void noOfPlayers() {
+        System.out.print("\nEnter number of players minimum 2 , maximum 4 : ");
+        int player = scanner.nextInt();
+        if (player >= 2 && player <= 4) {
+            System.out.println("\n" + player + " players will play the game");
+        } else {
+            System.out.println("Please enter number of players in the Range");
+            this.noOfPlayers();
+            scanner.close();
+        }
+    }
 }
--- src/com/bridgelabz/workshop/DeckOfCardsMain.java
+++ src/com/bridgelabz/workshop/DeckOfCardsMain.java
@@ -2,8 +2,12 @@ package com.bridgelabz.workshop;
 
 public class DeckOfCardsMain {
 	public static void main(String[] args) {
+		/**
+        In this I have created an object of DeckOfCardsGame class and just calling the method through object.
+         */
 		DeckOfCardsGame deckOfCardsGame = new DeckOfCardsGame();
 		deckOfCardsGame.welcome();
 		deckOfCardsGame.deckOfCards();
+		deckOfCardsGame.noOfPlayers();
 	}
 }
